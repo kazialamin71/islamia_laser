968014cf67ebfdcf959bf8d68989696f6ae538bd
kazialamin71 <kazialamin109@gmail.com>
2022-02-12 18:48:18 +0600

    collection report

--- islamia_reports/details_collection.py
+++ islamia_reports/details_collection.py
@@ -18,7 +18,10 @@ class report_detail_component(models.AbstractModel):
         products = self.env['product.product'].browse(data.get('ids', data.get('active_ids')))
         start_date = data['form']['date_start']
         end_date = data['form']['date_end']
-        query="select b.name,b.grand_total,p.name,m.paid_amount,m.p_type from investigation_bill b,patient_info p, money_receipt m where p.id=b.patient_name and b.id=m.bill_id and b.create_date>= %s and b.create_date<=%s group by b.name,b.grand_total,p.name,m.paid_amount,m.p_type"
+        query="select b.name,b.grand_total,p.name,m.paid_amount,m.p_type from investigation_bill b,patient_info p, money_receipt m " \
+              "where p.id=b.patient_name and b.id=m.bill_id and b.create_date>= %s and b.create_date<=%s" \
+              "and b.state='confirm' group by b.name,b.grand_total," \
+              "p.name,m.paid_amount,m.p_type order by b.name desc"
         self._cr.execute(query,[start_date,end_date])
         values=self._cr.fetchall()
         return {
